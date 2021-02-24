795a0570bd08ef652b4b7f9457abb067e105f8c3
Ibrahim <horlalekan123@gmail.com>
2021-02-24 16:49:37 +0100

    designing index

--- js/script.js
+++ js/script.js
@@ -1,22 +1,22 @@
-// const dropdown = document.getElementById("dropdown-wrapper");
-// const drop = document.getElementById("drop-link");
-// const span = document.getElementById("close");
-// drop.onmouseenter = function () {
-//   console.log("here");
-//   dropdown.classList.add("show");
-// };
-// window.onmouseenter = function (event) {
-//   console.log(event.target);
-//   if (event.target == dropdown) {
-//   }
-//   //   dropdown.classList.remove("show");
-// };
-// function hideDropdown() {
-//   dropdown.classList.remove("show");
-// }
+const dropdown = document.getElementById("dropdown-wrapper");
+const drop = document.getElementById("drop-link");
+const span = document.getElementById("close");
+drop.onmouseenter = function () {
+  console.log("here");
+  dropdown.classList.add("show");
+};
+drop.onmouseleave = function (event) {
+  console.log(event.target);
+  if (event.target == dropdown) {
+  }
+  dropdown.classList.remove("show");
+};
+function hideDropdown() {
+  dropdown.classList.remove("show");
+}
 
-// window.onclick = function (event) {
-//   if (event.target == dropdown) {
-//     dropdown.classList.remove("show");
-//   }
-// };
+window.onclick = function (event) {
+  if (event.target == dropdown) {
+    dropdown.classList.remove("show");
+  }
+};
